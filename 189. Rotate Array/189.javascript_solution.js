2042889ad5f55f9e43eb8efc839036bad451da02
Chris Li <chris.lty07@gmail.com>
2023-02-17 13:36:37 -0500

    Add solution explanation

--- 189. Rotate Array/189.javascript_solution.js
+++ 189. Rotate Array/189.javascript_solution.js
@@ -1,3 +1,12 @@
+/*
+    Solution: 
+    Copy the nums array
+    Iterate through the array. The element at index i + k (mod length of array) shall be the element at index i
+
+    Time Complexity: O(n)
+    Space Complexity: O(1)
+*/
+
 const rotate = (nums, k) => {
     const temp = [...nums];
     for (let i = 0; i < nums.length; i++) {
