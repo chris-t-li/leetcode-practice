efa4255f9492566a60083cd3457e5055a79ff58f
Chris Li <chris.lty07@gmail.com>
2023-02-20 15:11:58 -0500

    150 155 225 JS solutions

---
+++ 150. Evaluate Reverse Polish Notation/150.javascript_solution.js
@@ -0,0 +1,27 @@
+var evalRPN = function (tokens) {
+    const stack = [];
+    for (const char of tokens) {
+        switch (char) {
+            case "+":
+                stack[stack.length - 2] += stack[stack.length - 1];
+                stack.pop();
+                break;
+            case "-":
+                stack[stack.length - 2] -= stack[stack.length - 1];
+                stack.pop();
+                break;
+            case "*":
+                stack[stack.length - 2] *= stack[stack.length - 1];
+                stack.pop();
+                break;
+            case "/":
+                const value = stack[stack.length - 2] / stack[stack.length - 1];
+                stack[stack.length - 2] = value < 0 ? Math.ceil(value) : Math.floor(value);
+                stack.pop();
+                break;
+            default:
+                stack.push(parseInt(char));
+        }
+    }
+    return stack[0]
+};
---
+++ 225. Implement Stack using Queues/225.javascript_solution.js
@@ -0,0 +1,44 @@
+var MyStack = function () {
+    this.q = [];
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MyStack.prototype.push = function (x) {
+    this.q.push(x);
+};
+
+/**
+ * @return {number}
+ */
+MyStack.prototype.pop = function () {
+    for (let i = 0; i < this.q.length - 1; i++) {
+        this.q.push(this.q.shift())
+    }
+    return this.q.shift();
+};
+
+/**
+ * @return {number}
+ */
+MyStack.prototype.top = function () {
+    return this.q.slice(-1);
+};
+
+/**
+ * @return {boolean}
+ */
+MyStack.prototype.empty = function () {
+    return this.q.length == 0;
+};
+
+/** 
+ * Your MyStack object will be instantiated and called as such:
+ * var obj = new MyStack()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.empty()
+ */
